8bf1dbbad20dca0e762c382462922c9f5fb82f45
Kim Ji Soo <us13579@naver.com>
2022-05-21 23:54:00 +0900

    update
    Former-commit-id: a50945f081e7f62967e5805be343cea603ff07a0

--- frontend/src/components/board/BoardDetail.vue
+++ frontend/src/components/board/BoardDetail.vue
@@ -7,42 +7,70 @@
       </b-col>
       <b-col class="text-right">
         <b-button class="mr-2 button" @click="moveModify()">글 수정</b-button>
-        <b-button class="button">글 삭제</b-button>
+        <b-button class="button" @click="deleteBoard()">글 삭제</b-button>
       </b-col>
     </b-row>
-    <b-row class="mt-3 ml-2 mr-3 mb-1 border rounded border-4">
-      <b-col class="mt-3" style="text-align: left">
-        <b-form-group id="author-group" label="작성자" label-for="author">
+    <b-col class="mt-3 text-left" style="float: none; margin: 0 auto">
+      <b-row class="mt-2">
+        <b-form-group
+          class="col-md-4"
+          id="tag-group"
+          label="태그"
+          label-for="tag"
+        >
+          <b-form-input
+            id="tag"
+            type="text"
+            readonly
+            :value="`${board.tag_no}`"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          class="col-md-4"
+          id="author-group"
+          label="작성자"
+          label-for="author"
+        >
           <b-form-input
             id="author"
             type="text"
             readonly
-            placeholder="ssafy"
+            :value="`${board.user_id}`"
           ></b-form-input>
         </b-form-group>
-        <b-form-group id="date-group" label="작성일" label-for="date">
+        <b-form-group
+          class="col-md-4"
+          id="date-group"
+          label="작성일"
+          label-for="date"
+        >
           <b-form-input
             id="date"
-            type="date"
+            type="text"
             readonly
-            placeholder="날짜들어갈자리"
+            :value="`${board.registDate}` | dateFormat"
           ></b-form-input>
         </b-form-group>
+      </b-row>
+    </b-col>
+    <b-row>
+      <b-col class="mt-2" style="text-align: left">
         <b-form-group id="subject-group" label="제목" label-for="subject">
           <b-form-input
             id="subject"
             type="text"
             readonly
-            placeholder="제목들어갈자리"
+            :value="`${board.title}`"
           ></b-form-input>
         </b-form-group>
         <b-form-group id="content-group" label="내용" label-for="context">
           <b-form-textarea
             id="content"
             readonly
-            placeholder="내용들어갈자리"
-            rows="10"
-            max-rows="15"
+            :value="`${board.content}`"
+            rows="15"
+            max-rows="20"
           ></b-form-textarea>
         </b-form-group>
       </b-col>
@@ -51,8 +79,30 @@
 </template>
 
 <script>
+import { selectBoard, deleteBoard } from "@/api/board";
+import moment from "moment";
+
 export default {
   name: "BoardDetail",
+  data() {
+    return {
+      // 가져온 데이터 저장할 객체
+      board: {},
+    };
+  },
+  // 생성되자마자 데이터가져오기
+  created() {
+    console.log(this.$route.params.no);
+    selectBoard(
+      this.$route.params.no,
+      ({ data }) => {
+        this.board = data;
+      },
+      (error) => {
+        console.log("데이터 에러발생!!", error);
+      }
+    );
+  },
   methods: {
     // 리스트 페이지
     moveList() {
@@ -60,7 +110,27 @@ export default {
     },
     // 수정 페이지
     moveModify() {
-      this.$router.push({ name: "boardModify" });
+      this.$router.replace({
+        name: "boardModify",
+        params: { no: this.board.no },
+      });
+    },
+    // 글 삭제하기
+    deleteBoard() {
+      deleteBoard(
+        this.$route.params.no,
+        this.$router.replace({ name: "boardList" }),
+        (error) => {
+          console.log("삭제시 에러발생!!", error);
+        }
+      );
+    },
+  },
+
+  // 날짜 필터
+  filters: {
+    dateFormat(registDate) {
+      return moment(new Date(registDate)).format("YY년 MM월 DD일 H시 M분");
     },
   },
 };
--- frontend/src/components/board/item/BoardInputItem.vue
+++ frontend/src/components/board/item/BoardInputItem.vue
@@ -2,24 +2,69 @@
   <b-row class="mb-1">
     <b-col style="text-align: left">
       <b-form @submit="onSubmit" @reset="onReset">
-        <b-form-group id="author-group" label="작성자" label-for="author">
+        <!-- 태그 start -->
+
+        <!-- 글 작성 - 태그 -->
+
+        <b-form-group
+          id="tag-group"
+          label="태그"
+          label-for="tag"
+          v-if="this.type === 'register'"
+        >
+          <b-form-select
+            id="tag_no"
+            v-model="board.tag_no"
+            :options="options"
+            class="mt-1"
+          ></b-form-select>
+          <div class="mt-1">
+            Selected: <strong>{{ board.tag_no }}</strong>
+          </div>
+        </b-form-group>
+
+        <!-- 글 수정 - 태그 -->
+        <b-form-group id="tag-group" label="태그" label-for="tag" v-else>
+          <b-form-select
+            v-model="board.tag_no"
+            :options="options"
+            class="mt-1"
+          ></b-form-select>
+          <div class="mt-1">
+            Selected: <strong>{{ board.tag_no }}</strong>
+          </div>
+        </b-form-group>
+
+        <!-- 글 수정 - 태그 -->
+
+        <!-- 태그 end -->
+
+        <!-- 작성자 start -->
+
+        <b-form-group id="user_id-group" label="작성자" label-for="user_id">
           <b-form-input
-            id="author"
+            id="user_id"
+            v-model="board.user_id"
             type="text"
             readonly
             placeholder="ssafy"
           ></b-form-input>
         </b-form-group>
 
+        <!-- 작성자 end -->
+
+        <!-- 제목 start -->
+
         <!-- 글 작성 - 제목 -->
         <b-form-group
-          id="subject-group"
+          id="title-group"
           label="제목"
-          label-for="subject"
+          label-for="title"
           v-if="this.type === 'register'"
         >
           <b-form-input
-            id="subject"
+            id="title"
+            v-model="board.title"
             type="text"
             required
             placeholder="제목을 입력하세요"
@@ -27,19 +72,18 @@
         </b-form-group>
 
         <!-- 글 수정 - 제목 -->
-        <b-form-group
-          id="subject-group"
-          label="제목"
-          label-for="subject"
-          v-else
-        >
+        <b-form-group id="title-group" label="제목" label-for="title" v-else>
           <b-form-input
-            id="subject"
+            id="title"
+            v-model="board.title"
             type="text"
             required
-            placeholder="제목"
           ></b-form-input>
         </b-form-group>
+
+        <!-- 제목 end -->
+
+        <!-- 내용 start -->
 
         <!-- 글 작성 - 내용 -->
         <b-form-group
@@ -50,9 +94,10 @@
         >
           <b-form-textarea
             id="content"
+            v-model="board.content"
             placeholder="내용을 입력하세요"
             rows="10"
-            max-rows="15"
+            max-rows="20"
           ></b-form-textarea>
         </b-form-group>
 
@@ -65,12 +110,16 @@
         >
           <b-form-textarea
             id="content"
-            placeholder="내용"
+            v-model="board.content"
             required
             rows="10"
-            max-rows="15"
+            max-rows="20"
           ></b-form-textarea>
         </b-form-group>
+
+        <!-- 내용 end -->
+
+        <!-- 버튼 -->
 
         <b-button
           type="submit"
@@ -86,15 +135,135 @@
 </template>
 
 <script>
-//import http from "@/api/http";
+import { /*selectBoard,*/ insertBoard, modifyBoard } from "@/api/board";
 
 export default {
   name: "BoardInputItem",
   data() {
-    return {};
+    return {
+      // 가져온 데이터 저장할 객체
+      board: {
+        no: 0,
+        title: "",
+        content: "",
+        user_id: "ssafy",
+        data: "",
+        view: "",
+        tag_no: "",
+      },
+      tag_no: null,
+      options: [
+        { value: null, text: "태그를 선택해 주세요" },
+        { value: 1, text: "공지사항" },
+        { value: 2, text: "잡담" },
+        { value: 3, text: "꿀팁" },
+      ],
+    };
   },
   props: {
+    // 글 작성인지 수정인지 확인
     type: { type: String },
+  },
+  // 생성되자마자 데이터가져오기
+  created() {
+    //if (this.type === "modify"){
+    // selectBoard(
+    //   this.$route.params.no,
+    //   ({ data }) => {
+    //     this.board = data;
+    //   },
+    //   (error) => {
+    //     console.log("데이터 에러발생!!", error);
+    //   }
+    // );
+    //}else{} 사용자 정보 있는지 없는지 체크해서 해결
+  },
+  methods: {
+    // 버튼 눌렀을 때 작성하지 않은 부분에 커서
+    onSubmit(event) {
+      event.preventDefault();
+
+      let error = true;
+      let msg = "";
+      !this.board.tag_no && ((msg = "태그를 선택해주세요!"), (error = false));
+      error &&
+        !this.board.title &&
+        ((msg = "제목을 입력해주세요!"),
+        (error = false),
+        this.$ref.title.focus());
+      error &&
+        !this.board.content &&
+        ((msg = "내용을 입력해주세요!"),
+        (error = false),
+        this.$ref.content.focus());
+
+      // 만약 에러가 있을 경우 메세지 출력
+      if (!error) alert(msg);
+      // 에러가 없을 경우 입력 상태면 입력 함수를 수정 상태면 수정 함수를 호출
+      else {
+        this.type === "register" ? this.insertBoard() : this.modifyBoard();
+      }
+    },
+    onReset(event) {
+      event.preventDefault();
+      this.board.no = 0;
+      this.board.tag_no = 0;
+      this.board.title = "";
+      this.board.content = "";
+      this.$router.push({ name: "boardList" });
+    },
+    insertBoard() {
+      console.log(this.board);
+      insertBoard(
+        {
+          no: 0,
+          title: this.board.title,
+          content: this.board.content,
+          user_id: "ssafy",
+          date: "",
+          view: 0,
+          tag_no: this.board.tag_no,
+        },
+        ({ data }) => {
+          let msg = "등록 처리시 문제가 발생했습니다.";
+          if (data === "success") {
+            msg = "등록이 완료되었습니다.";
+          }
+          alert(msg);
+          this.moveList();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    modifyBoard() {
+      modifyBoard(
+        {
+          no: 0,
+          title: this.board.title,
+          content: this.board.content,
+          userid: "ssafy",
+          date: "",
+          view: 0,
+          tag_no: this.board.tag_no,
+        },
+        ({ data }) => {
+          let msg = "등록 처리시 문제가 발생했습니다.";
+          if (data === "success") {
+            msg = "등록이 완료되었습니다.";
+          }
+          alert(msg);
+          this.moveList();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    moveList() {
+      this.$router.push({ name: "boardList" });
+    },
   },
 };
 </script>
