83f94056c8bf38052e4486952bb87387656002ea
Kim Ji Soo <us13579@naver.com>
2022-05-21 23:41:27 +0900

    [#2] board API 연결
    Former-commit-id: e8683d053491f93945ba7cfabd3e5fae51fb659c

--- backend/src/main/java/com/ssafy/happy/dto/BoardDto.java
+++ backend/src/main/java/com/ssafy/happy/dto/BoardDto.java
@@ -1,15 +1,23 @@
 package com.ssafy.happy.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
-
+@ApiModel(value = "Board (게시글정보)", description = "글번호, 제목, 내용, 작성아이디, 작성일, 조회수, 태그번호 가진   Domain Class")
 public class BoardDto {
-	
+	@ApiModelProperty(value = "글번호")
 	private int no;
+	@ApiModelProperty(value = "제목")
 	private String title;
+	@ApiModelProperty(value = "내용")
 	private String content;
+	@ApiModelProperty(value = "작성아이디")
 	private String user_id;
+	@ApiModelProperty(value = "작성일")
 	private String registDate;
+	@ApiModelProperty(value = "조회수")
 	private int view;
+	@ApiModelProperty(value = "태그번호")
 	private String tag_no;
 	
 	
