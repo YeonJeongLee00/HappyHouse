f6015d5a63e293b0a3b439503e5a4976a685bb6d
Kim Ji Soo <us13579@naver.com>
2022-05-23 18:03:36 +0900

    [#5] 데이터추가, 뉴스 크롤링
    Former-commit-id: b238f644956d7f324703c1f0b5f76143cb78c826

--- backend/src/main/java/com/ssafy/vue/service/BoardServiceImpl.java
+++ backend/src/main/java/com/ssafy/vue/service/BoardServiceImpl.java
@@ -15,39 +15,39 @@ public class BoardServiceImpl implements BoardService {
 	private BoardMapper boardMapper;
 	
 	@Override
-	public List<BoardDto> listBoard() {
+	public List<BoardDto> listBoard() throws Exception {
 		return boardMapper.listBoard();
 	}
 
 	@Override
-	public BoardDto selectBoard(int articleno) {
+	public BoardDto selectBoard(int articleno) throws Exception {
 		return boardMapper.selectBoard(articleno);
 	}
 
 	@Override
-	public boolean insertBoard(BoardDto boardDto) {
+	public boolean insertBoard(BoardDto boardDto) throws Exception {
 		return boardMapper.insertBoard(boardDto) == 1;
 	}
 
 	@Override
 	@Transactional
-	public boolean updateBoard(BoardDto boardDto) {
+	public boolean updateBoard(BoardDto boardDto) throws Exception {
 		return boardMapper.updateBoard(boardDto) == 1;
 	}
 
 	@Override
 	@Transactional
-	public boolean deleteBoard(int articleno) {
+	public boolean deleteBoard(int articleno) throws Exception {
 		return boardMapper.deleteBoard(articleno) == 1;
 	}
 
 	@Override
-	public String selectTag(int no) {
+	public String selectTag(int no) throws Exception {
 		return boardMapper.selectTag(no);
 	}
 
 	@Override
-	public boolean updateView(int no) {
+	public boolean updateView(int no) throws Exception {
 		return boardMapper.updateView(no) == 1;
 	}
 	
